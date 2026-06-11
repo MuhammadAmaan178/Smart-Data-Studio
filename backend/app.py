d1e6ce68e9eabf7e419ca1f02c2d970ebc1bf091
MuhammadAmaan178 <mohammadamaanc178@gmail.com>
2026-06-11 15:47:55 +0500

    chore: run python app.py on port 7860 in debug mode inside Docker container

--- backend/app.py
+++ backend/app.py
@@ -288,4 +288,4 @@ def clear_clean_log():
     return jsonify({"success": True})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=7860, debug=True)
